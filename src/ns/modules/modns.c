ec970baca8f1f6dd68a8f63c83acbf27227302d7
darkopancev <darkopancev.dev@gmail.com>
2018-10-26 15:09:49 +0800

    [ns-modules] add modules base sourcefiles and modules directory

---
+++ src/ns/modules/modns.c
@@ -0,0 +1,29 @@
+#include "py/nlr.h"
+#include "py/runtime.h"
+
+#include <stdio.h>
+
+// Example usage to Hello objects
+//
+//      hello = ns.Hello("World")
+//      print (hello)
+
+STATIC mp_obj_t ns_hello(mp_obj_t what)
+{
+    printf("Hello %s!\n", mp_obj_str_get_str(what));
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(ns_hello_obj, ns_hello);
+
+STATIC const mp_rom_map_elem_t ns_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_ns) },
+    { MP_ROM_QSTR(MP_QSTR_Hello), (mp_obj_t)&ns_hello_obj },
+};
+
+STATIC MP_DEFINE_CONST_DICT(ns_module_globals, ns_module_globals_table);
+
+const mp_obj_module_t ns_module = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&ns_module_globals,
+};
