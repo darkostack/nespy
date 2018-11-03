d914e5f1370019bb6c73b6fa1d15c651e1554661
darkopancev <darkopancev.dev@gmail.com>
2018-11-03 21:36:56 +0800

    [ns] add more rpl commands

--- src/ns/modules/cli/cli.c
+++ src/ns/modules/cli/cli.c
@@ -38,13 +38,27 @@ static void command_version(int argc, char *argv[]);
 static void command_send(int argc, char *argv[]);
 static void command_ipaddr(int argc, char *argv[]);
 static void command_ip_neighbors(int argc, char *argv[]);
+#if UIP_CONF_IPV6_RPL
+static void command_rpl_set_root(int argc, char *argv[]);
+static void command_rpl_local_repair(int argc, char *argv[]);
+#if ROUTING_CONF_RPL_LITE
+static void command_rpl_refresh_routes(int argc, char *argv[]);
+#endif // ROUTING_CONF_RPL_LITE
+static void command_rpl_global_repair(int argc, char *argv[]);
+#endif // UIP_CONF_IPV6_RPL
+#if ROUTING_CONF_RPL_LITE
 static void command_rpl_status(int argc, char *argv[]);
 static void command_rpl_nbr(int argc, char *argv[]);
-static void command_rpl_set_root(int argc, char *argv[]);
+#endif // ROUTING_CONF_RPL_LITE
+static void command_routes(int argc, char *argv[]);
 
 // helper function
 static void cli_output_6addr(const uip_ipaddr_t *ipaddr);
 static void cli_output_lladdr(const linkaddr_t *lladdr);
+static const char * ds6_nbr_state_to_str(uint8_t state);
+static const char * rpl_state_to_str(enum rpl_dag_state state);
+static const char *rpl_mop_to_str(int mop);
+static const char *rpl_ocp_to_str(int ocp);
 
 static const ns_cli_cmd_t s_commands[] = {
     { "help", &command_help },
@@ -53,74 +67,20 @@ static const ns_cli_cmd_t s_commands[] = {
     { "send", &command_send },
     { "ip-addr", &command_ipaddr },
     { "ip-nbr", &command_ip_neighbors },
+#if UIP_CONF_IPV6_RPL
+    { "rpl-set-root", &command_rpl_set_root },
+    { "rpl-local-repair", &command_rpl_local_repair },
+#if ROUTING_CONF_RPL_LITE
+    { "rpl-refresh-routes", &command_rpl_refresh_routes },
+#endif // ROUTING_CONF_RPL_LITE
+    { "rpl-global-repair", &command_rpl_global_repair },
+#endif // UIP_CONF_IPV6_RPL
+#if ROUTING_CONF_RPL_LITE
     { "rpl-status", &command_rpl_status },
     { "rpl-nbr", &command_rpl_nbr },
-    { "rpl-set-root", &command_rpl_set_root },
+#endif // ROUTING_CONF_RPL_LITE
+    { "routes", &command_routes },
 };
-
-static const char * ds6_nbr_state_to_str(uint8_t state)
-{
-    switch (state) {
-    case NBR_INCOMPLETE:
-        return "Incomplete";
-    case NBR_REACHABLE:
-        return "Reachable";
-    case NBR_STALE:
-        return "Stale";
-    case NBR_DELAY:
-        return "Delay";
-    case NBR_PROBE:
-        return "Probe";
-    default:
-        return "Unknown";
-    }
-}
-
-#if ROUTING_CONF_RPL_LITE
-static const char * rpl_state_to_str(enum rpl_dag_state state)
-{
-    switch (state) {
-    case DAG_INITIALIZED:
-        return "Initialized";
-    case DAG_JOINED:
-        return "Joined";
-    case DAG_REACHABLE:
-        return "Reachable";
-    case DAG_POISONING:
-        return "Poisoning";
-    default:
-        return "Unknown";
-    }
-}
-
-static const char *rpl_mop_to_str(int mop)
-{
-    switch (mop) {
-    case RPL_MOP_NO_DOWNWARD_ROUTES:
-        return "No downward routes";
-    case RPL_MOP_NON_STORING:
-        return "Non-storing";
-    case RPL_MOP_STORING_NO_MULTICAST:
-        return "Storing";
-    case RPL_MOP_STORING_MULTICAST:
-        return "Storing+multicast";
-    default:
-        return "Unknown";
-    }
-}
-
-static const char *rpl_ocp_to_str(int ocp)
-{
-    switch (ocp) {
-    case RPL_OCP_OF0:
-        return "OF0";
-    case RPL_OCP_MRHOF:
-        return "MRHOF";
-    default:
-        return "Unknown";
-    }
-}
-#endif
 
 static void command_help(int argc, char *argv[])
 {
@@ -228,68 +188,7 @@ static void command_ip_neighbors(int argc, char *argv[])
     }
 }
 
-static void command_rpl_status(int argc, char *argv[])
-{
-#if ROUTING_CONF_RPL_LITE
-    cli_uart_output_format("RPL status:\r\n");
-    if (!curr_instance.used) {
-        cli_uart_output_format("-- Instance: None\r\n");
-    } else {
-        cli_uart_output_format("-- Instance: %u\r\n", curr_instance.instance_id);
-        if (NETSTACK_ROUTING.node_is_root()) {
-            cli_uart_output_format("-- DAG root\r\n");
-        } else {
-            cli_uart_output_format("-- DAG node\r\n");
-        }
-        cli_uart_output_format("-- DAG: ");
-        cli_output_6addr(&curr_instance.dag.dag_id);
-        cli_uart_output_format(", version %u\r\n", curr_instance.dag.version);
-        cli_uart_output_format("-- Prefix: ");
-        cli_output_6addr(&curr_instance.dag.prefix_info.prefix);
-        cli_uart_output_format("/%u\r\n", curr_instance.dag.prefix_info.length);
-        cli_uart_output_format("-- MOP: %s\r\n", rpl_mop_to_str(curr_instance.mop));
-        cli_uart_output_format("-- OF: %s\r\n", rpl_ocp_to_str(curr_instance.of->ocp));
-        cli_uart_output_format("-- Hop rank increment: %u\r\n", curr_instance.min_hoprankinc);
-        cli_uart_output_format("-- Default lifetime: %lu seconds\r\n",
-                               RPL_LIFETIME(curr_instance.default_lifetime));
-        cli_uart_output_format("-- State: %s\r\n", rpl_state_to_str(curr_instance.dag.state));
-        cli_uart_output_format("-- Preferred parent: ");
-        if (curr_instance.dag.preferred_parent) {
-            cli_output_6addr(rpl_neighbor_get_ipaddr(curr_instance.dag.preferred_parent));
-            cli_uart_output_format(" (last DTSN: %u)\r\n", curr_instance.dag.preferred_parent->dtsn);
-        } else {
-            cli_uart_output_format("None\r\n");
-        }
-        cli_uart_output_format("-- Rank: %u\r\n", curr_instance.dag.rank);
-        cli_uart_output_format("-- Lowest rank: %u (%u)\r\n",
-                               curr_instance.dag.lowest_rank, curr_instance.max_rankinc);
-        cli_uart_output_format("-- DTSN out: %u\r\n", curr_instance.dtsn_out);
-        cli_uart_output_format("-- DAO sequence: last sent %u, last acked %u\r\n",
-                               curr_instance.dag.dao_last_seqno, curr_instance.dag.dao_last_acked_seqno);
-        cli_uart_output_format("-- Trickle timer: current %u, min %u, max %u, redundancy %u\r\n",
-                               curr_instance.dag.dio_intcurrent, curr_instance.dio_intmin,
-                               curr_instance.dio_intmin + curr_instance.dio_intdoubl,
-                               curr_instance.dio_redundancy);
-    }
-#endif
-}
-
-static void command_rpl_nbr(int argc, char *argv[])
-{
-    if (!curr_instance.used || rpl_neighbor_count() == 0) {
-        cli_uart_output_format("RPL neighbors: none\r\n");
-    } else {
-        rpl_nbr_t *nbr = nbr_table_head(rpl_neighbors);
-        cli_uart_output_format("RPL neighbors:\r\n");
-        while (nbr != NULL) {
-            char buf[120];
-            rpl_neighbor_snprint(buf, sizeof(buf), nbr);
-            cli_uart_output_format("%s\r\n", buf);
-            nbr = nbr_table_next(rpl_neighbors, nbr);
-        }
-    }
-}
-
+#if UIP_CONF_IPV6_RPL
 static void command_rpl_set_root(int argc, char *argv[])
 {
     static int is_on;
@@ -338,6 +237,147 @@ static void command_rpl_set_root(int argc, char *argv[])
             cli_uart_output_format("Node is not a DAG root\r\n");
         }
     }
+}
+
+static void command_rpl_local_repair(int argc, char *argv[])
+{
+    cli_uart_output_format("Triggering routing local repair\r\n");
+    NETSTACK_ROUTING.local_repair("Cli");
+}
+
+#if ROUTING_CONF_RPL_LITE
+static void command_rpl_refresh_routes(int argc, char *argv[])
+{
+    cli_uart_output_format("Triggering routes refresh\r\n");
+    rpl_refresh_routes("Cli");
+}
+#endif // ROUTING_CONF_RPL_LITE
+
+static void command_rpl_global_repair(int argc, char *argv[])
+{
+    cli_uart_output_format("Triggering routing global repair\r\n");
+    NETSTACK_ROUTING.global_repair("Cli");
+}
+#endif // UIP_CONF_IPV6_RPL
+
+#if ROUTING_CONF_RPL_LITE
+static void command_rpl_status(int argc, char *argv[])
+{
+    cli_uart_output_format("RPL status:\r\n");
+    if (!curr_instance.used) {
+        cli_uart_output_format("-- Instance: none\r\n");
+    } else {
+        cli_uart_output_format("-- Instance: %u\r\n", curr_instance.instance_id);
+        if (NETSTACK_ROUTING.node_is_root()) {
+            cli_uart_output_format("-- DAG root\r\n");
+        } else {
+            cli_uart_output_format("-- DAG node\r\n");
+        }
+        cli_uart_output_format("-- DAG: ");
+        cli_output_6addr(&curr_instance.dag.dag_id);
+        cli_uart_output_format(", version %u\r\n", curr_instance.dag.version);
+        cli_uart_output_format("-- Prefix: ");
+        cli_output_6addr(&curr_instance.dag.prefix_info.prefix);
+        cli_uart_output_format("/%u\r\n", curr_instance.dag.prefix_info.length);
+        cli_uart_output_format("-- MOP: %s\r\n", rpl_mop_to_str(curr_instance.mop));
+        cli_uart_output_format("-- OF: %s\r\n", rpl_ocp_to_str(curr_instance.of->ocp));
+        cli_uart_output_format("-- Hop rank increment: %u\r\n", curr_instance.min_hoprankinc);
+        cli_uart_output_format("-- Default lifetime: %lu seconds\r\n",
+                               RPL_LIFETIME(curr_instance.default_lifetime));
+        cli_uart_output_format("-- State: %s\r\n", rpl_state_to_str(curr_instance.dag.state));
+        cli_uart_output_format("-- Preferred parent: ");
+        if (curr_instance.dag.preferred_parent) {
+            cli_output_6addr(rpl_neighbor_get_ipaddr(curr_instance.dag.preferred_parent));
+            cli_uart_output_format(" (last DTSN: %u)\r\n", curr_instance.dag.preferred_parent->dtsn);
+        } else {
+            cli_uart_output_format("none\r\n");
+        }
+        cli_uart_output_format("-- Rank: %u\r\n", curr_instance.dag.rank);
+        cli_uart_output_format("-- Lowest rank: %u (%u)\r\n",
+                               curr_instance.dag.lowest_rank, curr_instance.max_rankinc);
+        cli_uart_output_format("-- DTSN out: %u\r\n", curr_instance.dtsn_out);
+        cli_uart_output_format("-- DAO sequence: last sent %u, last acked %u\r\n",
+                               curr_instance.dag.dao_last_seqno, curr_instance.dag.dao_last_acked_seqno);
+        cli_uart_output_format("-- Trickle timer: current %u, min %u, max %u, redundancy %u\r\n",
+                               curr_instance.dag.dio_intcurrent, curr_instance.dio_intmin,
+                               curr_instance.dio_intmin + curr_instance.dio_intdoubl,
+                               curr_instance.dio_redundancy);
+    }
+}
+
+static void command_rpl_nbr(int argc, char *argv[])
+{
+    if (!curr_instance.used || rpl_neighbor_count() == 0) {
+        cli_uart_output_format("RPL neighbors: none\r\n");
+    } else {
+        rpl_nbr_t *nbr = nbr_table_head(rpl_neighbors);
+        cli_uart_output_format("RPL neighbors:\r\n");
+        while (nbr != NULL) {
+            char buf[120];
+            rpl_neighbor_snprint(buf, sizeof(buf), nbr);
+            cli_uart_output_format("%s\r\n", buf);
+            nbr = nbr_table_next(rpl_neighbors, nbr);
+        }
+    }
+}
+#endif // ROUTING_CONF_RPL_LITE
+
+static void command_routes(int argc, char *argv[])
+{
+    uip_ds6_defrt_t *default_route;
+
+    cli_uart_output_format("Default route:\r\n");
+    default_route = uip_ds6_defrt_lookup(uip_ds6_defrt_choose());
+    if (default_route != NULL) {
+        cli_uart_output_format("-- ");
+        cli_output_6addr(&default_route->ipaddr);
+        if (default_route->lifetime.interval != 0) {
+            cli_uart_output_format(" (lifetime: %lu seconds)\r\n",
+                    (unsigned long)default_route->lifetime.interval);
+        } else {
+            cli_uart_output_format(" (lifetime: infinite)\r\n");
+        }
+    } else {
+        cli_uart_output_format("-- none\r\n");
+    }
+
+#if UIP_CONF_IPV6_RPL
+    if (uip_sr_num_nodes() > 0) {
+        uip_sr_node_t *link;
+        cli_uart_output_format("Routing links (%u in total):\r\n", uip_sr_num_nodes());
+        link = uip_sr_node_head();
+        while (link != NULL) {
+            char buf[100];
+            uip_sr_link_snprint(buf, sizeof(buf), link);
+            cli_uart_output_format("-- %s\r\n", buf);
+            link = uip_sr_node_next(link);
+        }
+    } else {
+        cli_uart_output_format("No routing links\r\n");
+    }
+#endif // UIP_CONF_IPV6_RPL
+
+#if (UIP_MAX_ROUTES != 0)
+    if (uip_ds6_route_num_routes() > 0) {
+        uip_ds6_route_t *route;
+        cli_uart_output_format("Routing entries (%u in total):\r\n", uip_ds6_route_num_routes());
+        route = uip_ds6_route_head();
+        while (route != NULL) {
+            cli_uart_output_format("-- ");
+            cli_output_6addr(&route->ipaddr);
+            cli_uart_output_format(" via ");
+            cli_output_6addr(uip_ds6_route_nexthop(route));
+            if ((unsigned long)route->state.lifetime != 0xFFFFFFFF) {
+                cli_uart_output_format(" (lifetime: %lu seconds)\r\n", (unsigned long)route->state.lifetime);
+            } else {
+                cli_uart_output_format(" (lifetime: infinite)\r\n");
+            }
+            route = uip_ds6_route_next(route);
+        }
+    } else {
+        cli_uart_output_format("No routing entries\r\n");
+    }
+#endif // (UIP_MAX_ROUTES != 0)
 }
 
 void cli_process_line(char *buf, uint16_t buf_len)
@@ -422,3 +462,69 @@ static void cli_output_lladdr(const linkaddr_t *lladdr)
         }
     }
 }
+
+static const char * ds6_nbr_state_to_str(uint8_t state)
+{
+    switch (state) {
+    case NBR_INCOMPLETE:
+        return "Incomplete";
+    case NBR_REACHABLE:
+        return "Reachable";
+    case NBR_STALE:
+        return "Stale";
+    case NBR_DELAY:
+        return "Delay";
+    case NBR_PROBE:
+        return "Probe";
+    default:
+        return "Unknown";
+    }
+}
+
+#if ROUTING_CONF_RPL_LITE
+static const char * rpl_state_to_str(enum rpl_dag_state state)
+{
+    switch (state) {
+    case DAG_INITIALIZED:
+        return "Initialized";
+    case DAG_JOINED:
+        return "Joined";
+    case DAG_REACHABLE:
+        return "Reachable";
+    case DAG_POISONING:
+        return "Poisoning";
+    default:
+        return "Unknown";
+    }
+}
+
+static const char *rpl_mop_to_str(int mop)
+{
+    switch (mop) {
+    case RPL_MOP_NO_DOWNWARD_ROUTES:
+        return "No downward routes";
+    case RPL_MOP_NON_STORING:
+        return "Non-storing";
+    case RPL_MOP_STORING_NO_MULTICAST:
+        return "Storing";
+    case RPL_MOP_STORING_MULTICAST:
+        return "Storing+multicast";
+    default:
+        return "Unknown";
+    }
+}
+
+static const char *rpl_ocp_to_str(int ocp)
+{
+    switch (ocp) {
+    case RPL_OCP_OF0:
+        return "OF0";
+    case RPL_OCP_MRHOF:
+        return "MRHOF";
+    default:
+        return "Unknown";
+    }
+}
+#endif
+
+
