0ab309db6bdc864106711444331e9584490633aa
darkopancev <darkopancev.dev@gmail.com>
2018-11-13 18:16:42 +0800

    [ns] remove semicolllon in unix examples

--- examples/unix/coap-get/lib/hello.py
+++ examples/unix/coap-get/lib/hello.py
@@ -12,13 +12,13 @@ def get(res):
 def client_msg_callback(res):
     get_msg = res.get_payload()
     print(get_msg, "\r")
-    return;
+    return
 
 # hello thread functions
 def thread_begin():
     resource.client_ep("fd00::200:0:0:2")
     timer.start()
-    return;
+    return
 
 def thread_process(ev, data):
     if ev == event_begin:
@@ -27,7 +27,7 @@ def thread_process(ev, data):
         if (timer.expired()):
             resource.client_get("res/hello", client_msg_callback)
             timer.reset()
-    return;
+    return
 
 msg_counter = 0
 
--- examples/unix/coap-periodic/lib/hello.py
+++ examples/unix/coap-periodic/lib/hello.py
@@ -12,7 +12,7 @@ def get(res):
 def client_msg_callback(res):
     get_msg = res.get_payload()
     print(get_msg, "\r")
-    return;
+    return
 
 def periodic_callback(res):
     global periodic_counter
@@ -20,17 +20,17 @@ def periodic_callback(res):
     print("periodic callback:", periodic_counter, "\r")
     if periodic_counter % 4 == 0:
         res.notify_observers()
-    return;
+    return
 
 def thread_begin():
     resource.client_ep("fd00::200:0:0:2")
     resource.client_observe("res/hello", client_msg_callback)
-    return;
+    return
 
 def thread_process(ev, data):
     if ev == event_begin:
         thread_begin()
-    return;
+    return
 
 msg_counter = 0
 periodic_counter = 0
