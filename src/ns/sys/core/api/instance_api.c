0db35e22f4f7ca55cd77314d8f39c0fcc8f6f603
darkopancev <darkopancev.dev@gmail.com>
2018-11-16 18:50:24 +0800

    [ns-thread] simplify instance api functions

--- src/ns/sys/core/api/instance_api.c
+++ src/ns/sys/core/api/instance_api.c
@@ -3,14 +3,12 @@
 
 ns_instance_t ns_instance_init(void)
 {
-    ns_instance_t inst = (ns_instance_t)instance_init();
-    return inst;
+    return (ns_instance_t)instance_init();
 }
 
 ns_instance_t ns_instance_get(void)
 {
-    ns_instance_t inst = (ns_instance_t)instance_get();
-    return inst;
+    return (ns_instance_t)instance_get();
 }
 
 bool ns_instance_is_initialized(ns_instance_t instance)
