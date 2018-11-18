9786d69a8aa51207242b4ab12dbafb20d127bca0
darkopancev <darkopancev.dev@gmail.com>
2018-11-18 15:32:58 +0800

    [ns-thread] update timer python example to use 3 timer case

--- examples/unix/timer/main.py
+++ examples/unix/timer/main.py
@@ -9,13 +9,18 @@ def callback1():
 def callback2():
     print("timer2 expired\r")
 
+def callback3():
+    print("timer3 expired\r");
+
 timer1 = nespy.Timer(inst=instance, cb=callback1)
 timer2 = nespy.Timer(inst=instance, cb=callback2)
+timer3 = nespy.Timer(inst=instance, cb=callback3)
 
 def main():
     platform.system_init()
     timer1.start(1000)
     timer2.start(500)
+    timer3.start(1000)
     while True:
         platform.process_drivers(instance)
 
