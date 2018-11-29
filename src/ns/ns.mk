7e9086e653016f025b75fcbcdfe2cf5451db6e24
darkopancev <darkopancev.dev@gmail.com>
2018-11-29 17:30:05 +0800

    [ns-thread] modify ns.mk

--- src/ns/ns.mk
+++ src/ns/ns.mk
@@ -1,18 +1,33 @@
 # Network stack core makefile
 
-SRC_NS_CORE += $(addprefix ns/sys/core/,\
-    common/instance.c \
-    common/timer.c \
-    common/tasklet.c \
-    common/message.c \
-    common/encoding.c \
-    common/random.c \
-    utils/heap.c \
-    api/instance_api.c \
-    api/nstd_api.c \
-    api/tasklet_api.c \
-    api/message_api.c \
+SRC_NS_CORE_COMMON += $(addprefix ns/sys/core/common/,\
+    instance.c \
+    timer.c \
+    tasklet.c \
+    message.c \
+    encoding.c \
+    random.c \
     )
+
+SRC_NS_CORE_UTILS += $(addprefix ns/sys/core/utils/,\
+    heap.c \
+    )
+
+SRC_NS_CORE_MAC += $(addprefix ns/sys/core/mac/,\
+    mac_frame.c \
+    )
+
+SRC_NS_CORE_API += $(addprefix ns/sys/core/api/,\
+    instance_api.c \
+    nstd_api.c \
+    tasklet_api.c \
+    message_api.c \
+    )
+
+SRC_NS_CORE += $(SRC_NS_CORE_COMMON)
+SRC_NS_CORE += $(SRC_NS_CORE_UTILS)
+SRC_NS_CORE += $(SRC_NS_CORE_MAC)
+SRC_NS_CORE += $(SRC_NS_CORE_API)
 
 SRC_NS_CLI += $(addprefix ns/sys/cli/,\
     cli.c \
